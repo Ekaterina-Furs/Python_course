72f320520576fc2ce33de589312acac77855a1cb
EkaterinaFurs <mrs-katrina@mail.ru>
2023-06-26 00:59:06 +0400

    task_2_1_correct

--- Homework_2/hw_2_1.py
+++ Homework_2/hw_2_1.py
@@ -11,7 +11,7 @@ for i in range(num):
     if value == 1:
         count += 1
 if count < num/2:
-    print(count)
+    print(f"Минимальное количество монет = {count}")
 else:
-    print (num-count)
+    print (f"Минимальное количество монет = {num-count}")
 
