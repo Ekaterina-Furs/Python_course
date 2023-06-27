0165f900e9543c1360da0288129aab81c97a486f
EkaterinaFurs <mrs-katrina@mail.ru>
2023-06-27 23:52:32 +0400

    Add task_4_1 and task_4_2

--- Lesson_4/task_4_1.py
+++ Lesson_4/task_4_1.py
@@ -1,11 +1,14 @@
-#Решение в группах
-#Пользователь вводит текст(строка). Словом считается последовательность непробельных символов идущих подряд, 
-#слова разделены одним или большим числом пробелов. Определите, сколько различных слов содержится в этом тексте.
-#Input: She sells sea shells on the sea shore The shells that she sells are sea shells I'm sure.
-#So if she sells sea shells on the sea shore I'm sure that the shells are sea shore shells
+'''Напишите программу, которая принимает на вход строку, и отслеживает, сколько раз каждый символ уже встречался. Количество повторов добавляется к символам с помощью постфикса формата _n.
+Input: a a a b c a a d c d d
+Output: a a_1 a_2 b c a_3 a_4 d c_1 d_1 d_2
+Для решения данной задачи используйте функцию .split()'''
 
-text = input().split()
-set_result = set()
-for i in text:
-    set_result.add(i.lower())
-print(len(set_result))
+stroka = "a a a b c a a d c d d".split()
+print(stroka)
+result = {}
+for i in stroka:
+    if i in result:
+        print(f'{i}_{result[i]}', end=' ')
+    else:
+        print(i, end=' ')
+    result[i] = result.get(i, 0) + 1
