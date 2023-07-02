e48fa8c8d4b33d47610ee617f1b4c5e9be76867e
EkaterinaFurs <mrs-katrina@mail.ru>
2023-07-02 22:37:57 +0400

    Add conflict tasks

--- Homework_3/hw_3_1.py
+++ Homework_3/hw_3_1.py
@@ -2,18 +2,6 @@
 Пользователь в первой строке вводит натуральное число N – количество элементов в массиве. В последующих  строках записаны N целых чисел Ai.
 Последняя строка содержит число X.'''
 
-<<<<<<< HEAD
-n = int(input("Введите количество элементов: "))
-#list_1 = [1,2,3,3,5,3,2,2,2,2,2]
-list_1 = [int(input()) for i in range(n)]
-print (list_1)
-count = 0
-x = int(input("Введите искомый элемент: "))
-for i in range(len(list_1)):
-    if list_1[i] == x:
-            count += 1
-print (count)
-=======
 from random import randint as rd
 N = int(input("Введите количество элементов: "))
 min = int(input("Введите минимальное значение для заполнения списка: "))
@@ -31,4 +19,3 @@ for i in range(len(numbers)):
             count += 1
 print (count)
 
->>>>>>> 3_homework
